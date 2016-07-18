2b1cd245f69ad62e18a12952c138a66017239bef
Sneha Agrawal <snehaagrawal@apconic.net>
2016-07-18 15:04:10 +0530

    wwd

---
+++ app.js
@@ -0,0 +1,12 @@
+ function fibonacci(number){  
+  if(number === 1) {  
+    return [0, 1];  
+  }   
+  else {  
+   for(let i=1; i<=number; i++){
+          let fibonacci = fibonacci(number-1) + fibonacci(number-2);
+          return fibonacci;
+        }
+  }
+};  
+console.log(fibonacci(8)); 
