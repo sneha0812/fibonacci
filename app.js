a9b5ea06d614ea41e90e50ab6ef62120ca79a34d
Sneha Agrawal <snehaagrawal@apconic.net>
2016-07-18 16:41:56 +0530

    ugh

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
- function fibonacci(number){  
-  if(number === 1) {  
-    return [0, 1];  
-  }   
-  else {  
-   for(let i=1; i<=number; i++){
-          let fibonacci = fibonacci(number-1) + fibonacci(number-2);
-          return fibonacci;
-        }
+function fibonacci(number) {
+  if (number === 1) {
+    return [0, 1];
   }
-};  
+  for (let i = 1; i <= number; i++) {
+    let fibonacci = fibonacci(number - 1) + fibonacci(number - 2);
+    return fibonacci;
+  }
+};
 console.log(fibonacci(8)); 
